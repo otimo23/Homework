c7df40003da113536300697023512fa3724c47a6
olga Timoshenko <otimoshenko55@gmail.com>
2023-03-30 20:38:20 +0200

    программу,которая выведет все натуральные числа в промежутке от N до 1

---
+++ Rec01/Program.cs
@@ -0,0 +1,27 @@
+﻿/*  Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"  */
+
+using System;
+using static System.Console;
+Clear();
+
+
+int Prompt(string message)
+{
+    Write(message);
+    int result = int.Parse(ReadLine()!);
+    return result;
+}
+
+string PrintStringNum(int num, int firstnum)
+{
+    if (firstnum == num) return firstnum.ToString();
+    return (firstnum + " " + PrintStringNum(num, firstnum - 1));
+}
+
+int m = 1;
+int n = Prompt("Enter number N: ");
+
+WriteLine($"Natural numbers: {PrintStringNum(m, n)}");
+
